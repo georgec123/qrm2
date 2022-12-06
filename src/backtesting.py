28197ef6d6548bfd83b9fb10f6ba6b67035778dc
George Coxon <george.coxon@live.co.uk>
2022-12-06 21:08:40 +0000

    plots yah

---
+++ src/backtesting.py
@@ -0,0 +1,204 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from scipy.optimize import minimize
+from scipy.stats.distributions import chi2
+
+from typing import Tuple
+
+
+
+def LL_GPD(xi: float, beta: float, ys: np.ndarray) -> float:
+    # Part 4, slide 43
+    N = len(ys)
+
+    first = -N*np.log(beta)
+    second = (1+1/xi)*np.sum(np.log(1+ys*xi/beta))
+    return first - second
+
+def GPD_cdf(x: float, xi: float, beta: float) -> float: 
+    # Implement the CDF of GPD
+    # Part 4, slide 38
+    if xi == 0:
+        return 1 - np.exp(-x/beta)
+    else:
+        return 1 - (1 + xi * x / beta) ** (-1/xi)
+
+def empirical_F(dist: np.ndarray, x: float):
+    # assuming dist has a CDF of F, we return P(X<x) = F(x)
+    lt_x = dist[dist<x]
+    return len(lt_x)/len(dist)
+
+def q_alpha_pareto(zs: np.ndarray, alpha: float, xi: float, beta: float, u: float) -> np.ndarray:
+    # part 4, slide 62
+    q_alpha = u +  (beta/xi)*( ((1-alpha)/(1-empirical_F(zs, u)))**(-1*xi)  -1)
+
+    return q_alpha
+
+def es_pareto_mult(q: float, beta: float, xi: float, u: float) -> float:
+    # part 4, slide 62
+
+    return (q + beta-(xi*u))/(1-xi)
+
+
+def fit_gpd(dist: pd.Series, u: float ) -> Tuple[float, float]:
+    """
+    Fit a Generalised Pareto distribution
+
+    :param dist: Distrubution to fit GPD to
+    :param u: u to create excess distribution
+
+    :returns: xi, beta
+
+    """
+    excess_dist = dist[dist>u] - u
+
+    bnds = ((0.1,30), (0,30))
+
+    res = minimize(fun = lambda theta: -1*LL_GPD(theta[0], theta[1], ys=excess_dist),
+        x0 = (10,10),
+        method='SLSQP',
+        bounds=bnds,
+        options={'disp': False})
+
+    xi, beta = res.x
+    return xi, beta
+
+def compute_var_df(df: pd.DataFrame, q_95: float, q_99: float, es_95_mult: float, es_99_mult: float) -> pd.DataFrame:
+    
+    data = df.copy()
+    data['var_95'] = data['mu'] + data['std_forc']*q_95
+    data['var_99'] = data['mu'] + data['std_forc']*q_99
+
+    data['es_95'] = data['mu'] + data['std_forc']*es_95_mult
+    data['es_99'] = data['mu'] + data['std_forc']*es_99_mult
+    
+    return data
+
+
+def mean_excess(x: np.ndarray, u: float):
+    # ref Part 4 - slide 26
+    exceedances = x[x>u]
+    std_exceedances = exceedances-u
+    return np.mean(std_exceedances)
+
+
+def print_viols_and_plot(df: pd.DataFrame, title: str, q: str):
+    """
+    Print summary of data including: 99 VaR violations, 95 VaR violations, and VaR/ES plot
+    """
+    data = df.copy()
+    data.dropna(inplace=True)
+    ylabel = "Daily portfolio loss (%) (positive part)"
+    
+    ################ plot main chart################
+    ax = data[['max_loss']].plot(c='orange', linewidth=0.5, figsize=(10, 6))
+    data[['var_95','es_95','var_99','es_99']].plot(ax=ax, style=['r--','r-','b--','b-'], linewidth=0.5)
+
+
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel("Time")
+    
+    ax.xaxis.tick_bottom()
+    ax.yaxis.tick_left()
+    
+    ax.set_title(title)
+    ax.legend(loc='upper left')
+    plt.savefig(f'../plots/iv/iv_{q}i.png')
+
+    plt.show()
+
+    ################ plot var viols ################
+    for idx, var in enumerate(['95', '99']):
+        ax = data[['max_loss']].plot(c='orange', linewidth=0.5, figsize=(10, 6));
+        ax = data[[f'var_{var}']].plot(ax=ax, style=['b--'], linewidth=0.5)
+
+        viols = data[data['max_loss']>data[f'var_{var}']]
+        ax.scatter(viols.index,viols['max_loss'], marker='o', c='r', s=10, zorder=10)
+        ax.set_title(f"{title}. VaR {var}% violations")
+        ax.set_ylabel(ylabel)
+        ax.set_xlabel("Time")
+        plt.savefig(f'../plots/iv/iv_{q}{"i"*(idx+2)}.png')
+
+        plt.show()
+
+
+
+    num_days = (~data['var_95'].isna()).sum()
+    viols_95 = (data['loss']>data['var_95']).sum()
+    viols_99 = (data['loss']>data['var_99']).sum()
+
+    print(f"Violations 95%: {viols_95}, {100*viols_95/num_days:.2f}%")
+    print(f"Violations 99%: {viols_99}, {100*viols_99/num_days:.2f}%")
+
+
+def str_ci(x):
+    return str(x).split('.')[1]
+
+def log_lik(p: float, obs: pd.Series) -> float:
+    """
+    Likelihood ratio = -2*log(L_1/L_2)
+    This function will calculate log(L_i) for any series of binomial obesrvations
+
+    :param p: probability of success
+    :param obs: Series of observations, values are 1 or 0
+    """
+    return obs.apply(lambda x: np.log(((1-p)**(1-x))*(p**x))).sum()
+
+def lr_uc(alpha: float, exceeds: pd.Series) -> float:
+    """
+    Calculate unconditional likelihood ratio statistic for any series of binomial obesrvations
+
+    :param alpha: confidence level between 0 and 1
+    :param exceeds: Series of booleans/ints, indicating if the event happened
+    """
+    # calc pi_hat MLE 
+    obs = exceeds.astype(int)
+    pi_hat = np.mean(obs)
+
+    # return LR_UC for pi_hat and 1-alpha
+    return -2*(log_lik(1-alpha, obs)-log_lik(pi_hat, obs))
+
+def p_chi2(x: float, dof: int=1) -> float:
+    """
+    Calculate the p value for a chi2 distrubution
+
+    :param x: test statistic value
+    :param dof: Degrees of freedom for chi2 ist
+    """
+    return 1-chi2.cdf(x, dof)
+
+
+def get_stats(data: pd.DataFrame, title: str) -> pd.DataFrame: 
+    
+    df=pd.DataFrame(columns=['Alpha', 'Violations (exp)', 'LR_uc', 'LR_uc - p'])
+
+    for alpha in [0.95, 0.99]:
+
+        # str alpha for dataframe column lookup
+        str_alpha = str_ci(alpha)
+
+        # remove start data where there is no var
+        non_na_data = data[~data[f'var_{str_alpha}'].isna()]
+
+        num_days = len(non_na_data)
+        viol_mask = (non_na_data['loss']>non_na_data[f'var_{str_alpha}'])
+        num_viols = viol_mask.sum()
+
+
+        # calculate p values for var
+        expected_viols = (1-alpha)*num_days
+
+        likelihood_uc = lr_uc(alpha, viol_mask)
+        p_val_uc = p_chi2(likelihood_uc)
+
+        df.loc[len(df),:] = [
+                        alpha,
+                        f"{num_viols} ({expected_viols:.1f})",
+                        f"{likelihood_uc:.3f}",
+                        f"{p_val_uc:.5f}"
+                        ]
+                        
+    df.insert(0, 'Title', title)
+    return df
---
+++ src/plotting.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt 
+from statsmodels.tsa.stattools import acf
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+import scipy.stats as ss
+from scipy.stats.distributions import norm
+
+from backtesting import GPD_cdf
+
+def qqplot(x, dist="norm"):
+    fig, ax = plt.subplots(1,1)
+    t, o = ss.probplot(x, dist=dist, plot=ax)
+    
+    if isinstance(dist, str):
+        title = f"{dist.title()} Q-Q Plot"
+    else:
+        title = f"{dist.dist.name.title()} Q-Q Plot"
+        
+    
+    min_ = min(t[0])
+    max_ = max(t[0])
+    
+    ax.set_xlabel('Theoretical Quantiles')
+    ax.set_ylabel('Sample Quantiles')
+    ax.set_title(title)
+
+    ax.scatter(t[0],t[1])
+    ax.plot((min_, min_), (max_,max_))
+
+    return ax
+
+def get_linspace(data, num_stdevs=5, points=1001):
+    """
+    Return linspace of the data by calculating (mu-num_stdevs*std, mu+num_stdevs*std)
+    """
+    mean = np.mean(data)
+    stdev = np.std(data)
+
+    linspace = np.linspace(mean - num_stdevs*stdev, mean + num_stdevs*stdev, points)
+
+    return linspace
+
+
+def plot_acf_graphs(x: pd.Series, title: str):
+    fig, axs = plt.subplots(2,1)
+    ax1, ax2 = axs
+
+    ax1.set_title(title)
+    ax1.set_ylabel(title)
+    ax1.plot(x, c='b')
+
+    ax2.set_ylabel('ACF')
+    ax2.set_xlabel('Lag')
+
+    sm.graphics.tsa.plot_acf(x, ax=ax2, lags=50, title=None, auto_ylims=True, c='orange')
+
+
+def plot_excess_dist(y, xi, beta, u):
+    xx = np.linspace(0, 250, 10000) # For plotting the theoretical, fitted GPD
+
+    # Empirical CDF functionality from the "statsmodels" package
+    empirical_cdf_obj = sm.distributions.empirical_distribution.ECDF((y.to_numpy()).flatten()) 
+
+    fig, ax = plt.subplots()
+    ax.plot(xx, GPD_cdf(xx, xi, beta), 'k-', label='Fitted') # Plot the CDF of fitted GPD
+    ax.plot(y, empirical_cdf_obj(y), 'b*', label='Empirical') # Plot the empirical CDF of Y, the excess claim value above u
+    ax.legend()
+    ax.set_xscale('log')
+    ax.set_title(f'Empirical vs fitted CDF of the excess distribution, u={u}')
+    ax.set_ylabel('$F_{u}(x-u)$')
+    ax.set_xlabel('$x$')
+    return ax
+
+def histogram_w_kde_and_dist(x, title: str, dist=None, dist_name='Normal'):
+    if dist is None:
+        dist = norm()
+
+    linspace = get_linspace(x)
+    kde = ss.gaussian_kde(x)
+    dist_fit_pdf = dist.pdf(linspace)
+
+    fig, ax = plt.subplots()
+    ax.plot(linspace, dist_fit_pdf, label= f'{dist_name} Distribution Fit', color='red');
+    ax.plot(linspace, kde(linspace), label='KDE', color='k');
+    ax.hist(x, bins=50, density=True,facecolor='#2ab0ff', edgecolor='white');
+    ax.set_ylabel('Density')
+    ax.set_title(title)
+    plt.legend()
+    
+    return ax
